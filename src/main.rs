d9c5bf78d34257226adfd3e95bce729ed7731eb2
Ayush Bindlish <ayushbindllish@gmail.com>
2023-04-29 04:35:47 +0530

    Optimizations for faster execution on big directories

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,122 @@
 use std::collections::HashMap;
 use std::env;
+use std::error::Error;
 use std::fs::{self, metadata, File};
 use std::io::{self, Read};
+use std::num::NonZeroUsize;
+#[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
-use walkdir::WalkDir;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
 
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use ignore::WalkBuilder;
+use rayon::prelude::*;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let delete = args.contains(&String::from("--delete"));
-    let path = args.get(1).expect("Missing directory argument");
+use lru::LruCache;
 
-    let mut files_by_size: HashMap<u64, Vec<PathBuf>> = HashMap::new();
+type ChannelData = (String, PathBuf);
 
-    for entry in WalkDir::new(path) {
-        let entry = entry.unwrap();
-        if entry.path().is_file() {
-            let size = entry.metadata().unwrap().len();
-            let file_list = files_by_size.entry(size).or_insert_with(Vec::new);
-            file_list.push(entry.into_path());
-        }
+fn main() -> Result<(), Box<dyn Error>> {
+    let cache_size: NonZeroUsize = NonZeroUsize::new(16384).unwrap();
+    let args: Vec<String> = env::args().collect(); // Collect command line arguments
+    let delete = args.contains(&String::from("--delete")); // Check if --delete flag is set
+    let path = args.get(1).expect("Missing directory argument"); // Get directory path from arguments
+
+    let mut files_by_size: HashMap<u64, Vec<PathBuf>> = HashMap::new(); // Create a hash map to store files grouped by size
+
+    // Use WalkBuilder to create a directory iterator that respects .gitignore files
+    let walker = WalkBuilder::new(path)
+        .git_ignore(true)
+        .git_global(true)
+        .git_exclude(true)
+        .build();
+
+    for entry in walker
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().unwrap().is_file())
+    {
+        // Walk the directory tree, ignoring files and directories specified in .gitignore
+        let size = entry.metadata()?.len(); // Get the file size
+        let file_list = files_by_size.entry(size).or_insert_with(Vec::new); // Get or create a vector of files with this size
+        file_list.push(entry.path().to_owned()); // Add the file path to the vector
     }
 
-    let mut potential_duplicates: Vec<PathBuf> = Vec::new();
+    let mut potential_duplicates: Vec<PathBuf> = Vec::new(); // Create a vector to store potential duplicate files
 
     for (_, files) in files_by_size {
+        // For each size of files
         if files.len() > 1 {
-            potential_duplicates.extend(files);
+            // If there's more than one file with this size
+            potential_duplicates.extend(files); // Add all files to the potential duplicates vector
         }
     }
 
-    let mut duplicates: HashMap<String, Vec<PathBuf>> = HashMap::new();
+    let mut duplicates: HashMap<String, Vec<PathBuf>> = HashMap::new(); // Create a hash map to store duplicates grouped by hash
+    let hasher_cache: Arc<Mutex<LruCache<PathBuf, String>>> =
+        Arc::new(Mutex::new(LruCache::new(cache_size)));
 
-    for file_path in potential_duplicates {
-        let hash = hash_file(&file_path).unwrap();
-        let file_list = duplicates.entry(hash).or_insert_with(Vec::new);
-        file_list.push(file_path);
+    let (tx, rx): (Sender<ChannelData>, Receiver<ChannelData>) = channel(); // Create a message passing channel to send hashes and file paths between threads
+
+    potential_duplicates
+        .into_par_iter()
+        .for_each_with(tx.clone(), |tx, file_path| {
+            let hasher_cache = Arc::clone(&hasher_cache);
+            let mut hasher_cache = hasher_cache.lock().unwrap();
+            if let Some(cached_hash) = hasher_cache.get(&file_path) {
+                tx.send((cached_hash.clone(), file_path)).unwrap();
+            } else if let Ok(hash) = hash_file(&file_path) {
+                hasher_cache.put(file_path.clone(), hash.clone());
+                tx.send((hash, file_path)).unwrap(); // Send the hash and file path to the receiver
+            }
+        });
+
+    drop(tx); // Drop the sender so the receiver knows there are no more messages coming
+
+    for (hash, file_path) in rx {
+        // For each received message containing a hash and file path
+        let file_list = duplicates.entry(hash).or_insert_with(Vec::new); // Get or create a vector of files with this hash
+        file_list.push(file_path); // Add the file path to the vector
     }
 
-    let mut duplicate_count = 0;
-    let mut size_saved = 0;
+    let mut duplicate_count = 0; // Create a variable to count the number of duplicates
+    let mut size_saved = 0; // Create a variable to keep track of the amount of disk space saved
 
-    for (hash, files) in duplicates {
+    for (hash, mut files) in duplicates {
+        // For each hash of duplicate files
         if files.len() > 1 {
+            // If there's more than one file with this hash
             duplicate_count += files.len() - 1;
-            println!("Found {} files with hash {}:", files.len(), hash);
+
+            // Sort files by creation time (oldest first)
+            files.sort_by_key(|file| {
+                let metadata = metadata(file).unwrap();
+                metadata
+                    .created()
+                    .unwrap_or_else(|_| metadata.modified().unwrap())
+            });
+
+            println!("\n\n\nFound {} files with hash {}:\n", files.len(), hash);
             for (i, file) in files.iter().enumerate() {
                 if i == 0 {
-                    println!("  - {} [Original]", file.display());
+                    println!("  - {} [Original]\n", file.display());
                 } else {
-                    let file_size = metadata(file).unwrap().len();
+                    let file_size = metadata(file)?.len();
                     size_saved += file_size as usize;
 
                     if delete {
-                        let mut permissions = metadata(file).unwrap().permissions();
-                        permissions.set_mode(0o644);
-                        fs::set_permissions(file, permissions).unwrap();
-                        fs::remove_file(file).unwrap();
-                        println!("  - {} [Deleted]", file.display());
+                        #[cfg(unix)]
+                        {
+                            let mut permissions = metadata(file)?.permissions();
+                            permissions.set_mode(0o644);
+                            fs::set_permissions(file, permissions)?;
+                        }
+                        fs::remove_file(file)?;
+                        println!("  - {} [Deleted]\n", file.display());
                     } else {
-                        println!("  - {} [Duplicate]", file.display());
+                        println!("  - {} [Duplicate]\n", file.display());
                     }
                 }
             }
@@ -71,6 +125,7 @@ fn main() {
 
     println!("\n{} duplicates found.", duplicate_count);
     println!("{} bytes saved by deleting duplicates.", size_saved);
+    Ok(())
 }
 
 fn hash_file(file_path: &PathBuf) -> io::Result<String> {
